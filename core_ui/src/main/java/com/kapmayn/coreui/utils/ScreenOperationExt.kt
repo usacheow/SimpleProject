30d9fc503aceeb63c5759f24f58aa3cd86aeba7c
kapmayn <usacheow.ar@gmail.com>
2019-01-11 00:44:59 +0300

    added fragment manager extensions

--- core_ui/src/main/java/com/kapmayn/coreui/utils/ScreenOperationExt.kt
+++ core_ui/src/main/java/com/kapmayn/coreui/utils/ScreenOperationExt.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 inline fun <reified ACTIVITY> Context.intentOf(noinline block: (Intent.() -> Unit)? = null): Intent {
     return Intent(this, ACTIVITY::class.java).apply {
@@ -25,4 +28,28 @@ inline fun bundleOf(block: Bundle.() -> Unit): Bundle {
 
 fun <MODEL : Parcelable> Fragment.getParcelable(key: String): MODEL? {
     return arguments?.getParcelable(key)
+}
+
+inline fun FragmentManager.inTransaction(action: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().action().commit()
+}
+
+fun FragmentManager.createTransactionIn(
+    @IdRes containerId: Int,
+    fragment: Fragment,
+    needAddToBackStack: Boolean = true
+): FragmentTransaction {
+    val transaction = beginTransaction().replace(containerId, fragment, fragment::class.java.simpleName)
+    if (needAddToBackStack) {
+        transaction.addToBackStack(null)
+    }
+    return transaction
+}
+
+fun FragmentManager.showFragmentIn(
+    @IdRes containerId: Int,
+    toShowFragment: Fragment,
+    needAddToBackStack: Boolean = true
+) {
+    createTransactionIn(containerId, toShowFragment, needAddToBackStack).commit()
 }
