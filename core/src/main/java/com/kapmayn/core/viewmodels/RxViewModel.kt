0f2ae85d8a6f79f318b7ef6b25fd667f4e0b9797
kapmayn <usacheow.ar@gmail.com>
2019-01-15 16:47:03 +0300

    improved

--- core/src/main/java/com/kapmayn/core/viewmodels/RxViewModel.kt
+++ core/src/main/java/com/kapmayn/core/viewmodels/RxViewModel.kt
@@ -3,13 +3,11 @@ package com.kapmayn.core.viewmodels
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import org.reactivestreams.Subscription
-import javax.inject.Inject
 
 abstract class RxViewModel : ViewModel() {
 
-    @Inject
-    lateinit var disposables: CompositeDisposable
-    val subscriptions = mutableListOf<Subscription>()
+    protected var disposables = CompositeDisposable()
+    protected var subscriptions = mutableListOf<Subscription>()
 
     override fun onCleared() {
         super.onCleared()
