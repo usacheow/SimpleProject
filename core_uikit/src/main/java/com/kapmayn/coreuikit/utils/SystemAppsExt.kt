629d2833e033f28f6b4c19bec17b699ebcf78a1b
kapmayn <usacheow.ar@gmail.com>
2019-06-21 15:10:18 +0300

    added custom tabs

--- core_uikit/src/main/java/com/kapmayn/coreuikit/utils/SystemAppsExt.kt
+++ core_uikit/src/main/java/com/kapmayn/coreuikit/utils/SystemAppsExt.kt
@@ -3,7 +3,9 @@ package com.kapmayn.coreuikit.utils
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.kapmayn.coreuikit.R
 
 fun Fragment.openMapsApp(latitude: Double, longitude: Double) {
     val uri = Uri.parse("geo:<$latitude>,<$longitude>?q=<$latitude>,<$longitude>(Вам сюда)")
@@ -35,4 +37,12 @@ fun Fragment.openEmailApp(mail: String, subject: String) {
     val uri = Uri.parse(feedbackTheme)
     val intent = Intent(Intent.ACTION_VIEW).setData(uri)
     context?.startActivity(intent)
+}
+
+fun Fragment.openCustomTab(url: String) {
+    CustomTabsIntent.Builder()
+        .setShowTitle(true)
+        .setToolbarColor(ContextCompat.getColor(context!!, R.color.colorPrimary))
+        .build()
+        .launchUrl(context, Uri.parse(url))
 }
