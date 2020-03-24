b11520fb34e2bb93224f97f8030ba36338bc1e09
Artyom <usacheow.ar@gmail.com>
2020-03-24 21:16:29 +0300

    added core_unit_test module and test example

---
+++ authorization/src/test/java/com/usacheow/authorization/domain/AuthInteractorTest.kt
@@ -0,0 +1,57 @@
+package com.usacheow.authorization.domain
+
+import com.usacheow.authorization.data.AuthRepository
+import com.usacheow.authorization.data.models.AccessToken
+import com.usacheow.coredata.database.Storage
+import com.usacheow.coreunittest.RxRule
+import io.reactivex.Single
+import io.reactivex.observers.TestObserver
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnit
+
+class AuthInteractorTest {
+
+    @Rule @JvmField val rule = MockitoJUnit.rule()
+    @Rule @JvmField val rxRule = RxRule()
+
+    private val repository = mock(AuthRepository::class.java)
+    private val storage = mock(Storage::class.java)
+    private val interactor = AuthInteractor(repository, storage)
+
+    @Test
+    fun `should return complete on success response`() {
+        //given
+        val login = "login"
+        val password = "password"
+        val token = "token"
+        val observer = TestObserver<Unit>()
+        val response = Single.just(AccessToken(token))
+        `when`(repository.signInWithLoginAndPassword(login, password)).thenReturn(response)
+        //when
+        interactor.signInWithLoginAndPassword(login, password, observer)
+        //then
+        verify(repository).signInWithLoginAndPassword(login, password)
+        verify(storage).token = token
+        observer.assertComplete()
+    }
+
+    @Test
+    fun signUpWithLoginAndPassword() {
+    }
+
+    @Test
+    fun signInWithPhone() {
+    }
+
+    @Test
+    fun resendCode() {
+    }
+
+    @Test
+    fun verifyPhone() {
+    }
+}
