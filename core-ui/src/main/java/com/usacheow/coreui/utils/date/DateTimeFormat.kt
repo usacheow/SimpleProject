318c608f30d9d23607be74566b238a2b863bd76f
Усачев Артемий <artemiy.usachev@gazprombank.ru>
2021-08-31 14:05:37 +0300

    update

--- core-ui/src/main/java/com/usacheow/coreui/utils/date/DateTimeFormat.kt
+++ core-ui/src/main/java/com/usacheow/coreui/utils/date/DateTimeFormat.kt
@@ -10,7 +10,9 @@ enum class DateTimeFormat(val code: String) {
     ISO_LOCAL_TIME("HH:mm:ss"),
 
     ISO_LOCAL_DATE_TIME("yyyy-MM-dd'T'HH:mm[:ss][.SSS]"),
+    //XXX - 03:00
     ISO_OFFSET_DATE_TIME("yyyy-MM-dd'T'HH:mm[:ss][.SSS][XXX]"),
+    //Z - 0300
     ISO_ZONED_DATE_TIME("yyyy-MM-dd'T'HH:mm[:ss][.SSS]Z");
 
     fun toDateTimeFormat(locale: Locale = LOCALE) = code.toDateTimeFormat(locale)
