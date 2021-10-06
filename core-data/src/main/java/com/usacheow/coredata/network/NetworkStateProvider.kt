4cbb9e65abc4872dadad54bad2aaaa2708760228
Усачев Артемий <artemiy.usachev@gazprombank.ru>
2021-10-06 09:02:26 +0300

    updated NetworkStateProvider

--- core-data/src/main/java/com/usacheow/coredata/network/NetworkStateProvider.kt
+++ core-data/src/main/java/com/usacheow/coredata/network/NetworkStateProvider.kt
@@ -3,17 +3,13 @@ package com.usacheow.coredata.network
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkRequest
-import com.usacheow.coredata.coroutine.ApplicationCoroutineScope
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class NetworkStateProvider @Inject constructor(
-    @ApplicationCoroutineScope private val scope: CoroutineScope,
     connectivityManager: ConnectivityManager,
 ) {
 
@@ -23,16 +19,12 @@ class NetworkStateProvider @Inject constructor(
     private val networkStateCallback = object : ConnectivityManager.NetworkCallback() {
         override fun onAvailable(network: Network) {
             super.onAvailable(network)
-            scope.launch {
-                _state.emit(true)
-            }
+            _state.tryEmit(true)
         }
 
         override fun onLost(network: Network) {
             super.onLost(network)
-            scope.launch {
-                _state.emit(false)
-            }
+            _state.tryEmit(false)
         }
     }
 
