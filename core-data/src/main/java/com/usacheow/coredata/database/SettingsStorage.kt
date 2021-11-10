d45164a7e4df83de69118d24cce70008e2911834
Усачев Артемий <artemiy.usachev@gazprombank.ru>
2021-11-10 18:42:33 +0300

    updated settings storage

--- core-data/src/main/java/com/usacheow/coredata/database/SettingsStorage.kt
+++ core-data/src/main/java/com/usacheow/coredata/database/SettingsStorage.kt
@@ -24,25 +24,25 @@ class SettingsStorage @Inject constructor(
     var uiMode: UiMode
         get() = preferenceManager.getString(PREF_UI_MODE, "").run { UiMode.get(this) }
         set(value) = preferenceManager.edit {
-            putString(PREF_UI_MODE, value.name).apply()
+            putString(PREF_UI_MODE, value.name)
         }
 
     var isAllowBiometric: Boolean
         get() = preferenceManager.getBoolean(PREF_ALLOW_FINGERPRINT, true)
         set(value) = preferenceManager.edit {
-            putBoolean(PREF_ALLOW_FINGERPRINT, value).apply()
+            putBoolean(PREF_ALLOW_FINGERPRINT, value)
         }
 
     var isFirstEntry: Boolean
         get() = preferenceManager.getBoolean(PREF_IS_FIRST_ENTRY, true)
         set(value) = preferenceManager.edit {
-            putBoolean(PREF_IS_FIRST_ENTRY, value).apply()
+            putBoolean(PREF_IS_FIRST_ENTRY, value)
         }
 
     var lastCheckedAvailableVersionCode: Int
         get() = preferenceManager.getInt(PREF_LAST_CHECKED_AVAILABLE_VERSION_CODE, 0)
         set(value) = preferenceManager.edit {
-            putInt(PREF_LAST_CHECKED_AVAILABLE_VERSION_CODE, value).apply()
+            putInt(PREF_LAST_CHECKED_AVAILABLE_VERSION_CODE, value)
         }
 }
 
