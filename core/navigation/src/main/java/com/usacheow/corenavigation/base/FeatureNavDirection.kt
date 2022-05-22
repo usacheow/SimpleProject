dca8872b5e333eef86a84cc8a4a46e0cec28cee7
Усачев Артемий <artemiy.usachev@gazprombank.ru>
2022-05-22 17:07:18 +0300

    updated routes

--- core/navigation/src/main/java/com/usacheow/corenavigation/base/FeatureNavDirection.kt
+++ core/navigation/src/main/java/com/usacheow/corenavigation/base/FeatureNavDirection.kt
@@ -6,12 +6,12 @@ import com.usacheow.corenavigation.RouteWithArg
 data class FeatureNavDirection<ARG>(
     val route: RouteWithArg,
     val clazz: Class<ARG>,
-    val arg: ARG? = null,
+    val arg: ARG,
     val optionsBuilder: NavOptionsBuilder.() -> Unit = {},
 )
 
 inline fun <reified ARG> FeatureNavDirection(
     route: RouteWithArg,
-    arg: ARG? = null,
+    arg: ARG,
     noinline optionsBuilder: NavOptionsBuilder.() -> Unit = {},
 ) = FeatureNavDirection(route, ARG::class.java, arg, optionsBuilder)
