aa0028ef48473186dce52ab50af6613cd43e220a
Artemy <usacheow.ar@gmail.com>
2023-05-15 13:03:49 +0300

    remove showcase modules

--- showcase/billing/src/main/java/com/usacheow/showcasebilling/model/Product.kt
+++
@@ -1,7 +0,0 @@
-package com.usacheow.showcasebilling.model
-
-import com.android.billingclient.api.SkuDetails
-
-data class Product(
-    val details: SkuDetails,
-)
--- showcase/utils/src/main/java/com/usacheow/showcaseutils/featuretoggle/Feature.kt
+++
@@ -1,8 +0,0 @@
-package com.usacheow.showcaseutils.featuretoggle
-
-enum class Feature(
-    val key: String = "",
-    val defaultValue: Boolean = true
-) {
-    UNKNOWN
-}
